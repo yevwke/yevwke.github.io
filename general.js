f86d379bc905a46d287788b1538ff82d051dabd8
yevwke <117420729+yevwke@users.noreply.github.com>
2026-08-15 07:08:37 +0300

    upd

--- general.js
+++ general.js
@@ -1,7 +1,14 @@
 
 
 document.addEventListener("DOMContentLoaded", () => {
-    
+    const user = "yev" + "wke";
+    const domain = "gm" + "ail";
+    const email = user + "@" + domain + "." + "com";
+    const emailLink = document.getElementById("email-link");
+    if (emailLink) {
+        emailLink.href = "mailto:" + email;
+    }
+
     renderProjects();
     initBackground();
 });
